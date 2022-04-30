84e56028ea7c86e2115c7f38638c5bd1bd8a5203
YishengChew <64354150+YishengCheww@users.noreply.github.com>
2022-04-30 09:55:42 +0800

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,80 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Interior'
+version '1.1.0'
+
+this_is_a_map 'yes'
+
+client_scripts {
+	'client/main.lua',
+	--'client/optional.lua'
+}
+
+files {
+	'starter_shells_k4mb1.ytyp',
+	'stream/pinkcage/gabz_pinkcage.ytyp',
+	-- 'stream/ClassicHouseShells/shellpropsv19.ytyp',
+	-- 'stream/DeluxeHousingShells/shellpropsv2.ytyp',
+	-- 'stream/FurnishedHousingShells/shellpropsv11.ytyp',
+	-- 'stream/FurnishedMotelsShells/shellpropsv18.ytyp',
+	-- 'stream/FurnishedStashhousesShells/shellpropsv15.ytyp',
+	-- 'stream/GarageShells/shellpropsv8.ytyp',
+	-- 'stream/HighendHousingShells/shellpropsv9.ytyp',
+	-- 'stream/HighendLabShells/kshellsdrug.ytyp',
+	-- 'stream/LabShells/shellpropsv7.ytyp',
+	-- 'stream/MediumHousingShells/shellpropsv10.ytyp',
+	-- 'stream/ModernHotelsShells/shellpropsv14.ytyp',
+	-- 'stream/ModernHousingShells/shellpropsv12.ytyp',
+	-- 'stream/OfficeShells/shellpropsv3.ytyp',
+	-- 'stream/StashhousesShells/shellpropsv16.ytyp',
+	-- 'stream/StoreShells/shellpropsv4.ytyp',
+	-- 'stream/WarehouseShells/shellpropsv5.ytyp',
+	-- 'stream/AllShellsBundle/allshellsbundle.ytyp',
+	-- 'stream/AllFurnishedShellsBundle/allfurnishedshellsbundle.ytyp',
+	-- 'stream/AllEmptyShellsBundle/allemptyshellsbundle.ytyp',
+}
+
+-- Default (included)
+data_file 'DLC_ITYP_REQUEST' 'starter_shells_k4mb1.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/pinkcage/gabz_pinkcage.ytyp'
+-- Classic
+-- data_file 'DLC_ITYP_REQUEST' 'stream/ClassicHouseShells/shellpropsv19.ytyp'
+-- -- Deluxe
+-- data_file 'DLC_ITYP_REQUEST' 'stream/DeluxeHousingShells/shellpropsv2.ytyp'
+-- -- Furnished Housing
+-- data_file 'DLC_ITYP_REQUEST' 'stream/FurnishedHousingShells/shellpropsv11.ytyp'
+-- -- Furnished Motels
+-- data_file 'DLC_ITYP_REQUEST' 'stream/FurnishedMotelsShells/shellpropsv18.ytyp'
+-- -- Furnished Stash
+-- data_file 'DLC_ITYP_REQUEST' 'stream/FurnishedStashhousesShells/shellpropsv15.ytyp'
+-- -- Garage
+-- data_file 'DLC_ITYP_REQUEST' 'stream/GarageShells/shellpropsv8.ytyp'
+-- -- High End
+-- data_file 'DLC_ITYP_REQUEST' 'stream/HighendHousingShells/shellpropsv9.ytyp'
+-- -- High End Labs
+-- data_file 'DLC_ITYP_REQUEST' 'stream/HighendLabShells/kshellsdrug.ytyp'
+-- -- Labs
+-- data_file 'DLC_ITYP_REQUEST' 'stream/LabShells/shellpropsv7.ytyp'
+-- -- Medium Housing
+-- data_file 'DLC_ITYP_REQUEST' 'stream/MediumHousingShells/shellpropsv10.ytyp'
+-- -- Modern Hotels
+-- data_file 'DLC_ITYP_REQUEST' 'stream/ModernHotelsShells/shellpropsv14.ytyp'
+-- -- Modern Housing
+-- data_file 'DLC_ITYP_REQUEST' 'stream/ModernHousingShells/shellpropsv12.ytyp'
+-- -- Office
+-- data_file 'DLC_ITYP_REQUEST' 'stream/OfficeShells/shellpropsv3.ytyp'
+-- -- Stash
+-- data_file 'DLC_ITYP_REQUEST' 'stream/StashhousesShells/shellpropsv16.ytyp'
+-- -- Store
+-- data_file 'DLC_ITYP_REQUEST' 'stream/StoreShells/shellpropsv4.ytyp'
+-- -- Warehouse
+-- data_file 'DLC_ITYP_REQUEST' 'stream/WarehouseShells/shellpropsv5.ytyp'
+-- -- All Shells Bundle
+-- data_file 'DLC_ITYP_REQUEST' 'stream/AllShellsBundle/allshellsbundle.ytyp'
+-- -- All Furnished Bundle
+-- data_file 'DLC_ITYP_REQUEST' 'stream/AllFurnishedShellsBundle/allfurnishedshellsbundle.ytyp'
+-- -- All Empty Bundle
+-- data_file 'DLC_ITYP_REQUEST' 'stream/AllEmptyShellsBundle/allemptyshellsbundle.ytyp'
+
+lua54 'yes'
